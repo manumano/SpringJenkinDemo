aaa9bf9512b0c10c105065fac23153461743c85b
arunkanth85 <arun1shines@gmail.com>
2021-11-16 14:38:28 +0000

    First commit

---
+++ SpringJenkinDemo/src/main/java/com/example/demo/SpringJenkinDemoApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringJenkinDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringJenkinDemoApplication.class, args);
+	}
+
+}
